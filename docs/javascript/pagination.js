d62ab4010ee214b76a03fffe767fe4583ecc3e7d
zvidmarb <zvidmarb@ucsd.edu>
2025-10-22 12:29:39 -0700

    Fixed implementation; see note about page button listeners

--- docs/javascript/pagination.js
+++ docs/javascript/pagination.js
@@ -13,25 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updatePagination() {
         // Update page states
-        pages.forEach((btn, i) => btn.classList.toggle("selected", i === currentIndex));
+        pages.forEach((page, i) => page.classList.toggle("selected", i === currentIndex));
         
         // Disable controls for bounding pages
         prevControl.classList.toggle("disabled", currentIndex === 0);
         nextControl.classList.toggle("disabled", currentIndex === pages.length - 1);
     }
 
-    //Click listeners
+    //Control button listeners
     prevControl.addEventListener("click", () => {
         //in case of CSS class but no HTML attribute
         if (prevControl.classList.contains("disabled")) return; 
-        currentIndex++;
+        currentIndex--;
         updatePagination();
     });
     nextControl.addEventListener("click", () => {
         //in case of CSS class but no HTML attribute
         if (nextControl.classList.contains("disabled")) return; 
-        currentIndex--;
+        currentIndex++;
         updatePagination();
+    });
+    // Page button listeners, consider refactor for dynamic/ lots of pages
+    pages.forEach( (page, i) => {
+        page.addEventListener("click", () => {
+        currentIndex = i;
+        updatePagination();
+        });
     });
 
     updatePagination();
